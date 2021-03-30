b8c6d1b6e706450205c78209d9e6af07c6c7b8a3
Piyarat Tharaporn <Piyarat@MacBookPro-Piyarat.local>
2021-03-31 03:33:10 +0700

    commit

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-// Reply using AIML, parsing data with AIMLParser
+// Reply using AIML
 
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
-const AIMLParser = require('aimlparser')
+const AIMLInterpreter = require('aimlinterpreter')
 
 const app = express()
 const port = process.env.PORT || 4000
-const aimlParser = new AIMLParser({ name:'HelloBot' })
+const aimlInterpreter = new AIMLInterpreter({ name:'HelloBot'})
 
-aimlParser.load(['./test-aiml.xml'])
+aimlInterpreter.loadAIMLFilesIntoArray(['./test-aiml.xml'])
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -17,7 +17,7 @@ app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
     let msg = req.body.events[0].message.text
-    aimlParser.getResult(msg, (answer, wildCardArray, input) => {
+    aimlInterpreter.findAnswerInLoadedAIMLFiles(msg, (answer, wildCardArray, input) => {
         reply(reply_token, answer)
     })
     res.sendStatus(200)
