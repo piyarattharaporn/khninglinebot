39d5d37ff404675ed6f4c1148132240e3fc20f57
Piyarat Tharaporn <Piyarat@MacBookPro-Piyarat.local>
2021-03-31 03:23:08 +0700

    3 commit

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Echo reply
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
@@ -7,11 +9,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
-    reply(reply_token)
+    let msg = req.body.events[0].message.text
+    reply(reply_token, msg)
     res.sendStatus(200)
 })
 app.listen(port)
-function reply(reply_token) {
+function reply(reply_token, msg) {
     let headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer {m3Ke76tZcHwlkq1Nk0m8CHYRlv0RTMRuCul/HLLpSNpznORYlNycYwZlWx0fdaE6sotmTx8KXLJtsOOBZ3L47kOGF4TWQcFGKk3ACKHzpZ3Kq/7kAyOTvNt8dFy3xFpXjgsHkUNXu9YvrJBHeySAjQdB04t89/1O/w1cDnyilFU=}'
@@ -20,11 +23,7 @@ function reply(reply_token) {
         replyToken: reply_token,
         messages: [{
             type: 'text',
-            text: 'Hello'
-        },
-        {
-            type: 'text',
-            text: 'How are you?'
+            text: msg
         }]
     })
     request.post({
