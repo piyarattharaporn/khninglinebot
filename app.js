c4b38852c4bbac6bced8ab19b178fb62d3d4c451
Piyarat Tharaporn <Piyarat@MacBookPro-Piyarat.local>
2021-03-31 12:45:07 +0700

    commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
-const AIMLInterpreter = require('./AIMLInterperter')
+const AIMLInterpreter = require('aimlinterperter')
 
 
 const app = express()
